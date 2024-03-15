0520878ece4b76b560e8ca850c397f198bf8903a
Jr Gouveia <gouvik.dev@gmail.com.br>
2024-03-15 02:26:51 -0300

    feat: adiciona validação dos campos de usuário no método de criação

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,6 +1,7 @@
 import { UsersRepository } from "../repositories/user.repository";
 import { BadRequestException } from "./../exceptions/badRequest.exception";
 import { ICreate } from "./../interfaces/user.interface";
+import { UserValidator } from "./validators/user.validator";
 
 class UserService {
   private userRepository: UsersRepository;
@@ -11,12 +12,19 @@ class UserService {
 
   async create({ name, email, cpf, status, phone }: ICreate) {
     try {
-      return await this.userRepository.create({
+      const data = UserValidator.validateCreateUser({
         name,
         email,
         cpf,
         status,
         phone,
+      });
+      return await this.userRepository.create({
+        name: data.name,
+        email: data.email,
+        cpf: data.cpf,
+        status: data.status,
+        phone: data.phone,
       });
     } catch (error: any) {
       if (error.code === "P2002") {
