b5a6580c84508d68f1ae05fd4b78aefed9551d12
Jr Gouveia <gouvik.dev@gmail.com.br>
2024-03-15 23:19:43 -0300

    fix: percebido o erro. Os esquemas não podem ser unificados por conta da opcionalidade da edição

--- backend/src/services/validators/user.validator.ts
+++ backend/src/services/validators/user.validator.ts
@@ -4,7 +4,7 @@ import { BadRequestException } from "../../exceptions/badRequest.exception";
 import { ICreate } from "../../interfaces/user.interface";
 
 class UserValidator {
-  static createAndUpdateUserSchema = z.object({
+  static createUserSchema = z.object({
     name: z
       .string({
         required_error: "Nome é obrigatório",
@@ -33,11 +33,49 @@ class UserValidator {
 
   static validateCreateUser = (data: ICreate) => {
     try {
-      return this.createAndUpdateUserSchema.parse(data);
+      return this.createUserSchema.parse(data);
     } catch (error: any) {
       throw new BadRequestException(error.errors[0].message);
     }
   };
+
+  static validateUpdateUser = (data: ICreate) => {
+    try {
+      return this.updateUserSchema.parse(data);
+    } catch (error: any) {
+      throw new BadRequestException(error.errors[0].message);
+    }
+  };
+
+  static updateUserSchema = z.object({
+    name: z
+      .string()
+      .min(3, { message: "O nome está muito curto" })
+      .max(78, { message: "O nome está muito longo" })
+      .regex(/^[a-zA-Z\s]+$/, {
+        message: "Nome deve conter somente letras",
+      })
+      .optional(),
+    email: z.string().email().optional(),
+    cpf: z
+      .string()
+      .length(11, { message: "CPF deve ter somente 11 números" })
+      .regex(/^\d+$/, { message: "CPF deve conter somente números" })
+      .optional(),
+    phone: z
+      .string()
+      .length(11, { message: "Telefone deve ter somente 11 números" })
+      .regex(/^\d+$/, { message: "Telefone deve conter somente números" })
+      .optional(),
+    status: z
+      .enum([
+        StatusEnum.ACTIVE,
+        StatusEnum.INACTIVE,
+        StatusEnum.DEACTIVATED,
+        StatusEnum.PENDING,
+      ])
+      .optional(),
+  });
 }
 
 export { UserValidator };
