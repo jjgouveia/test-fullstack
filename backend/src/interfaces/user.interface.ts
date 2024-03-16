f8e9975d899c38e7e211bf1c532e98743a563cfe
Jr Gouveia <gouvik.dev@gmail.com.br>
2024-03-15 23:27:00 -0300

    fix: atualiza interface de atualização de usuários para permitir campos opcionais

--- backend/src/interfaces/user.interface.ts
+++ backend/src/interfaces/user.interface.ts
@@ -1,13 +1,13 @@
 import StatusEnum from "../enums/status.enum";
 
 export interface ICreate {
-  name: string;
+  name?: string;
   email: string;
   cpf: string;
   status: StatusEnum;
   phone: string;
 }
 
-export interface IUpdate extends ICreate {
+export interface IUpdate extends Partial<ICreate> {
   id: string;
 }
