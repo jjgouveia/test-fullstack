6c80856a5c158ad9c18e8801f476ca33040aa64a
Jr Gouveia <gouvik.dev@gmail.com.br>
2024-03-17 03:23:49 -0300

    frat: adiciona componente de loading para a lista de clientes em carregamento

---
+++ frontend/app/components/LoadingDots/index.tsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function LoadingDots() {
+  return (
+    <div className="dots-container__wrapper">
+      <div className="dots-container">
+        <div className="dots text-amber-400"></div>
+        <div className="dots text-amber-400"></div>
+        <div className="dots text-amber-400"></div>
+        <div className="dots text-amber-400"></div>
+        <div className="dots text-amber-400"></div>
+      </div>
+    </div>
+  );
+}
