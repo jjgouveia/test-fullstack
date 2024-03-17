9160174059d5ceba5dd4b235bc12a26a555a1bc1
Jr Gouveia <gouvik.dev@gmail.com.br>
2024-03-17 16:07:49 -0300

    feat: adiciona método PATCH às permissões de acesso do servidor

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,7 +20,7 @@ class App {
       res.header("Access-Control-Allow-Origin", "*");
       res.header(
         "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS"
+        "GET, POST, PUT, DELETE, OPTIONS, PATCH"
       );
       res.header("Access-Control-Allow-Headers", "*");
       next();
