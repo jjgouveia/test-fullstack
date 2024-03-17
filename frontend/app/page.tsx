e8f180639315cceb0905ea9ba30082e8bda55dc7
Jr Gouveia <gouvik.dev@gmail.com.br>
2024-03-17 03:24:45 -0300

    feat: implementações na página inicial

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,33 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { getCustomers } from "./services/api";
+
+import Button from "./components/Button";
+import ClientCard from "./components/ClientCard";
+import CreateCustomerForm from "./components/ClientUserForm";
+import Header from "./components/Header";
+import LoadingDots from "./components/LoadingDots";
+import CustomerProps from "./types/customer.type";
 
 export default function Home() {
+  const [users, setUsers] = useState<CustomerProps[]>([]);
+  const [openCreateArea, setOpenCreateArea] = useState(false);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  useEffect(() => {
+    getCustomers().then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
   return (
     <div className="flex w-full min-h-screen flex-col">
-      <header className="flex items-end justify-center bg-gray-950 backdrop-blur-xl h-[98px]">
-        <h1 className="px-4 text-4xl font-bold flex w-full items-center bg-gray-900 text-center justify-center h-[76px] text-white">
-          UOL
-        </h1>
-      </header>
-
+      <Header />
       <div className="flex w-full flex-1 justify-center mt-0 p-4 sm:mt-32 sm:p-0">
         <main className="flex flex-col w-full sm:w-4/5 p-4 bg-gray-100 rounded-lg shadow-lg">
           <div className="flex justify-center sm:justify-start align-middle">
@@ -35,52 +54,48 @@ export default function Home() {
             </div>
             <div className="flex align-middle items-center mr-0 sm:mr-4">
               <div className="flex justify-between items-center">
-                <button className="p-2 bg-amber-600 text-white rounded-md">
-                  Novo Cliente
-                </button>
+                <Button
+                  onClick={() => {
+                    setOpenCreateArea(!openCreateArea);
+                  }}
+                >
+                  {openCreateArea ? "Cancelar" : "Criar Cliente"}
+                </Button>
               </div>
             </div>
           </div>
+          <div id="create_user_container" className="mt-8">
+            {openCreateArea ? (
+              <CreateCustomerForm
+                users={users}
+                setUsers={setUsers}
+                setOpenCreateArea={setOpenCreateArea}
+              />
+            ) : null}
+          </div>
           <section id="list_users_container" className="mt-8">
             <ul>
-              <li className="flex justify-between items-center p-4 bg-gray-50 border border-gray-200">
-                <div className="flex items-center gap-4">
-                  <Image
-                    className="rounded-full"
-                    src="/users.svg"
-                    alt="UOL"
-                    width={32}
-                    height={32}
-                  />
-                  <div className="flex flex-col">
-                    <h4 className="text-lg font-bold text-gray-900">
-                      Jarbas Gouveia
-                    </h4>
-                    <span className="text-gray-500">jarbas@gouveia.com</span>
-                  </div>
+              {users && users.length > 0 ? (
+                users.map(({ id, cpf, email, name, phone, status }) => {
+                  return (
+                    <ClientCard
+                      key={id}
+                      id={id}
+                      cpf={cpf}
+                      email={email}
+                      name={name}
+                      phone={phone}
+                      status={status}
+                      users={users}
+                      setUsers={setUsers}
+                    />
+                  );
+                })
+              ) : (
+                <div className="flex items-center">
+                  <LoadingDots />
                 </div>
-                <div className="flex items-center gap-4">
-                  <div className="flex flex-col">
-                    <h4 className="text-lg font-bold text-gray-900">
-                      123.456.789-00
-                    </h4>
-                    <span className="text-gray-500">(81) 99612-2536</span>
-                  </div>
-                </div>
-                <div className="flex items-center gap-4">
-                  <div className="flex flex-col">
-                    <h4 className="text-gray-500">🟢 Ativo</h4>
-                  </div>
-                </div>
-                <div className="flex gap-4">
-                  <button className="p-2 bg-amber-600 text-white rounded-md">
-                    Editar
-                  </button>
-                  <button className="p-2 bg-red-600 text-white rounded-md">
-                    Excluir
-                  </button>
-                </div>
-              </li>
+              )}
             </ul>
           </section>
         </main>
